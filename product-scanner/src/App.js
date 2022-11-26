bf67f7c258599e562505b46bf1547393d6c36333
Vivek Prasad <Vivekpsd01@gmail.com>
2022-11-26 13:27:42 +0530

    Integrating qr-scanner and built QRScanner Component

--- product-scanner/src/App.js
+++ product-scanner/src/App.js
@@ -1,26 +1,14 @@
 import {useState} from 'react'
 import QRCode from 'react-qr-code'
+import QrScanner from 'qr-scanner';
+import {} from 'qr-scanner'
+import QRScanner from './components/QRScanner'
 
 function App() {
-
-  const [inputValue, setInputValue] = useState("");
-
   return (
     <div className="App">
-      <h1>QR Code Generator and Downloader</h1>
-
-      <div style={{ height: "auto", margin: "0 auto", maxWidth: 64, width: "100%" }}>
-        <QRCode
-          size={256}
-          style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-          value={inputValue}
-          viewBox={`0 0 256 256`}
-        />
-      </div>
-
-      <input type="text" onChange={(e) => {
-        setInputValue(e.target.value)
-      }}></input>
+      <h1>hello.....</h1>
+      <QRScanner />
     </div>
   );
 }
