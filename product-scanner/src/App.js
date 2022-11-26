9129b999756e4e40fd8a73fe6f00d641b67d0223
Vivek Prasad <Vivekpsd01@gmail.com>
2022-11-26 19:26:15 +0530

    Safe Commit

--- product-scanner/src/App.js
+++ product-scanner/src/App.js
@@ -1,15 +1,11 @@
-import {useState} from 'react'
-import QRCode from 'react-qr-code'
-import QrScanner from 'qr-scanner';
 import {} from 'qr-scanner'
 import QRScanner from './components/QRScanner'
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello.....</h1>
-      <QRScanner />
-    </div>
+      <>
+        <QRScanner />
+      </>
   );
 }
 
